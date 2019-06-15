fae4c989727f94be69f5f99c9fb099f128930f22
vunguyenvu35 <vunguyenvu35@gmail.com>
2019-06-15 13:08:58 +0700

    update

--- centos7/run-httpd.sh
+++ centos7/run-httpd.sh
@@ -2,8 +2,7 @@
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
-# exec php-fpm
-exec php-fpm -F
+# exec php-fpm -F
 # exec httpd
 rm -f /usr/local/apache2/logs/httpd.pid
-exec /usr/local/apache2/bin/httpd -DFOREGROUND
+# exec /usr/local/apache2/bin/httpd -DFOREGROUND
